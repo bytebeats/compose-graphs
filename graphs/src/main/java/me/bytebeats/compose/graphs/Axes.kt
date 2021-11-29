66e6fcef367b17b81b2c48842566375678ce5c92
Chen Pan <happychinapc@gmail.com>
2021-11-29 11:44:55 +0800

    Initial commit

---
+++ graphs/src/main/java/me/bytebeats/compose/graphs/Axes.kt
@@ -0,0 +1,19 @@
+package me.bytebeats.compose.graphs
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * Created by bytebeats on 2021/11/29 : 11:33
+ * E-mail: happychinapc@gmail.com
+ * Quote: Peasant. Educated. Worker
+ */
+@Composable
+internal fun GraphYAxis(
+    modifier: Modifier,
+    paddingTop: Float,
+    paddingBottom: Float,
+    scale: Float,
+    content: @Composable () -> Unit
+) {
+}
