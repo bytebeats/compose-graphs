e3df3f508c173358c8bec81c4af6c89b8ce804b7
Chen Pan <happychinapc@gmail.com>
2021-12-02 11:32:06 +0800

    it works.

--- app/src/main/java/me/bytebeats/compose/graphs/app/MainActivity.kt
+++ app/src/main/java/me/bytebeats/compose/graphs/app/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import me.bytebeats.compose.graphs.app.line.*
 import me.bytebeats.compose.graphs.app.ui.theme.ComposeGraphsTheme
 import me.bytebeats.compose.graphs.app.viewmodel.MainViewModelImpl
 
@@ -26,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LineGraphs(viewModel = viewModel)
                 }
             }
         }
@@ -34,14 +40,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeGraphsTheme {
-        Greeting("Android")
+fun LineGraphs(viewModel: MainViewModelImpl) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        itemsIndexed(viewModel.lines.value) { index, lines ->
+            when (index) {
+                0 -> LineGraph2(lines = lines)
+                1 -> Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 15.dp)
+                ) {
+                    LineGraph4(
+                        lines = lines,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                }
+                2 -> LineGraph1(lines = lines)
+                3 -> LineGraph3(lines = lines)
+                4 -> LineGraph5(lines = lines)
+            }
+        }
     }
 }
