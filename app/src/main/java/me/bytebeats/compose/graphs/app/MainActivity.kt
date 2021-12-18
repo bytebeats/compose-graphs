ce546449b2fb44b16d24de0165ecb8926b8aa199
Chen Pan <happychinapc@gmail.com>
2021-12-18 18:33:37 +0800

    product flavors.

--- app/src/main/java/me/bytebeats/compose/graphs/app/MainActivity.kt
+++ app/src/main/java/me/bytebeats/compose/graphs/app/MainActivity.kt
@@ -1,20 +1,20 @@
 package me.bytebeats.compose.graphs.app
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import me.bytebeats.compose.graphs.app.line.*
 import me.bytebeats.compose.graphs.app.ui.theme.ComposeGraphsTheme
@@ -41,23 +41,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LineGraphs(viewModel: MainViewModelImpl) {
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        itemsIndexed(viewModel.lines.value) { index, lines ->
-            when (index) {
-                0 -> LineGraph2(lines = lines)
-                1 -> Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 15.dp)
-                ) {
-                    LineGraph4(
-                        lines = lines,
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val curContext = LocalContext.current
+        val info = curContext.packageManager.getPackageInfo(
+            curContext.packageName,
+            PackageManager.GET_ACTIVITIES
+        )
+        Text(text = "$Server, ${info.packageName}, ${info.versionName}, ${info.versionCode}")
+        LazyColumn(modifier = Modifier.fillMaxWidth()) {
+            itemsIndexed(viewModel.lines.value) { index, lines ->
+                when (index) {
+                    0 -> LineGraph2(lines = lines)
+                    1 -> Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 15.dp)
+                    ) {
+                        LineGraph4(
+                            lines = lines,
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
+                    }
+                    2 -> LineGraph1(lines = lines)
+                    3 -> LineGraph3(lines = lines)
+                    4 -> LineGraph5(lines = lines)
                 }
-                2 -> LineGraph1(lines = lines)
-                3 -> LineGraph3(lines = lines)
-                4 -> LineGraph5(lines = lines)
             }
         }
     }
---
+++ app/src/production/kotlin/me/bytebeats/compose/graphs/app/Consts.kt
@@ -0,0 +1,9 @@
+package me.bytebeats.compose.graphs.app
+
+/**
+ * Created by bytebeats on 2021/12/18 : 18:20
+ * E-mail: happychinapc@gmail.com
+ * Quote: Peasant. Educated. Worker
+ */
+
+internal const val Server = "Product Server"
